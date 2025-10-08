f8a84e5b480fd8f295bcb2614702263b9c2637c7
LOGANATHANTECH <vloganathantech@gmail.com>
2025-10-08 14:43:33 +0530

    added frontend domain id to cors for security purpos

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ const PORT = process.env.PORT || 5000;
 // CORS Configuration
 
 const allowedOrigins = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
+    'https://ecommercedashbored.netlify.app',
 ];
 const corsOptions = {
     origin: function (origin, callback) {
